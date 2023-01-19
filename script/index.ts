bf97818fcc1a163d8fc8abb0825be8a61ed4dac8
timstackblock <49165468+timstackblock@users.noreply.github.com>
2023-01-19 09:54:03 -0500

    Delete script directory

--- script/index.ts
+++
@@ -1,9 +0,0 @@
-import { triggerAnchorBlockTests } from "./tests/anchor.blocks.spec";
-
-const anchorBlockOptions = {
-  accountId: "ST3C4YVASFG37P8E6210J9KHZJB4MF8ZRBQFKH41N",
-  recipient: "ST297VG59W96DPGBT13SGD542QE1XS954X4ZSWW2J",
-};
-
-// trigger anchor block testing script
-triggerAnchorBlockTests(anchorBlockOptions);
